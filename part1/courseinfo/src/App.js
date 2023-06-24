54384a85af28e23da79e99a2623e2aeef5705c37
Teklit Gebrerufael <teklit.gebrer@gmail.com>
2023-06-24 00:46:49 -0700

    course information, step2

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -6,12 +6,24 @@ const Header= (props)=>{
     </div>
   )
 }
+
+const Part = (props)=>{
+  return (
+  <div>
+    <p>{props.part1} {props.exercise1}</p>
+  </div>
+  )
+}
+
 const Content= (props)=>{
+  const par = 'Fundamentals of React' ;
+  const exe = 10 ;
+  
   return (
     <div>
-      <p>{props.part1} {props.exercises1}</p>
-      <p>{props.part2} {props.exercises2}</p>
-      <p>{props.part3} {props.exercises3}</p>
+      <Part part1={par} exercise1={exe}/>
+      <Part part1={par} exercise1={exe}/>
+      <Part part1={par} exercise1={exe} />
     </div>
   )
 }
@@ -26,17 +38,17 @@ const Total=(props)=>{
 
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
+ 
   const exercises1 = 10
-  const part2 = 'Using props to pass data'
+
   const exercises2 = 7
-  const part3 = 'State of a component'
+  
   const exercises3 = 14
 
   return (
     <div>
       <Header course={course}/>
-      <Content part1={part1} part2={part2} part3={part3} exercises1={exercises1} exercises2={exercises2} exercises3={exercises3} />
+      <Content />
       <Total exercises1={exercises1} exercises2={exercises2} exercises3={exercises3} />
     </div>
   )
