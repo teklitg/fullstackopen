8e05bb9cda173e5109f4ad86ec7d10d24c799920
Teklit Gebrerufael <teklit.gebrer@gmail.com>
2023-07-11 01:43:31 -0700

    2.3*: Course information step8

--- part2/courseinfo/src/App.js
+++ part2/courseinfo/src/App.js
@@ -1,10 +1,24 @@
 const App = () => {
+  const Header=()=>{
+    return <h1>{course.name}</h1>
+  }
+
+  const Total=()=>{
+
+    const total=course.parts.reduce((sum, part) => sum + part.exercises, 0)
+    return (
+      <div>
+        <p> total of {total} exercises</p>
+      </div>
+    )
+  }
 
   const Course=({course})=>{
 
+
     return (
       <div>
-        <h1>{course.name}</h1>
+        <Header course={course}/>
         <ul>
           {
             course.parts.map(part=><li key={part.id}>
@@ -12,11 +26,11 @@ const App = () => {
               </li>)  
           }
         </ul>
-
-        <h3>total of {course.parts[0].exercises +course.parts[1].exercises+course.parts[2].exercises} exercises</h3>
+        <Total />
       </div>
     )
   }
+
   const course = {
     id: 1,
     name: 'Half Stack application development',
@@ -38,6 +52,7 @@ const App = () => {
       }
     ]
   }
+
 
   return <Course course={course} />
 }
