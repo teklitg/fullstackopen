f857fb27fb1ac24350d465fe673a488e2f870f08
teklitg <teklit.gebrer@gmail.com>
2023-08-25 05:55:26 -0700

    2.8: The Phonebook Step3

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,28 +1,32 @@
 import { useState } from 'react'
 
 const App = () => {
-  const [persons, setPersons] = useState([{ name: 'Arto Hellas' }]) 
+  const [persons, setPersons] = useState([{ name: 'Arto Hellas', number: '39-44-5323523' }]) 
   const [newName, setNewName] = useState('')
+  const [newNumber, setNumber] =useState('')
 
-  const addName=(e)=>{
+  const addNewPerson=(e)=>{
     e.preventDefault()
     
   persons.find(person=>person.name===newName)? 
     window.alert(newName + " is already added to phonebook")
-   :setPersons(persons.concat({name: newName}));
+   :setPersons(persons.concat({name: newName, number: newNumber}));
   setNewName("")
+  setNumber("")
   }
 
+  const handleChange =setValue=> (e) => setValue(e.target.value)
 
-  const handleNameChange = (e) => {
-    setNewName(e.target.value)
-  }
+  
   return (
     <div>
       <h2>Phonebook</h2>
-      <form onSubmit={addName}>
+      <form onSubmit={addNewPerson}>
         <div>
-          name: <input value={newName} onChange={handleNameChange}/>
+          name: <input value={newName} onChange={handleChange(setNewName)}/>
+        </div>
+        <div>
+          number: <input value={newNumber} onChange={handleChange(setNumber)}/>
         </div>
         <div>
           <button type="submit">add</button>
@@ -31,7 +35,7 @@ const App = () => {
       <h2>Numbers</h2>
       <div>
         {persons.map(person => 
-          <div key={person.name}>{person.name}</div> 
+          <div key={person.name}>{person.name} {person.number}</div> 
         )}
       </div>
     </div>
