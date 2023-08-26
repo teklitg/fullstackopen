dfd0bb45caaefdff999486dcb83ba4b6add2a22e
teklitg <teklit.gebrer@gmail.com>
2023-08-26 00:12:08 -0700

    step4 part2

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,7 +1,13 @@
 import { useState } from 'react'
 
 const App = () => {
-  const [persons, setPersons] = useState([{ name: 'Arto Hellas', number: '39-44-5323523' }]) 
+ 
+ const [persons, setPersons] = useState([
+      { name: 'Arto Hellas', number: '040-123456', id: 1 },
+      { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+      { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+      { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
+    ])
   const [newName, setNewName] = useState('')
   const [newNumber, setNumber] =useState('')
   const [filter, setFilter] = useState('')
