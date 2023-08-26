dfa0809e0aaef57dd80f60830fe5c9ea88079b87
teklitg <teklit.gebrer@gmail.com>
2023-08-26 00:04:49 -0700

    2.9*: The Phonebook Step4

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -4,13 +4,14 @@ const App = () => {
   const [persons, setPersons] = useState([{ name: 'Arto Hellas', number: '39-44-5323523' }]) 
   const [newName, setNewName] = useState('')
   const [newNumber, setNumber] =useState('')
+  const [filter, setFilter] = useState('')
 
   const addNewPerson=(e)=>{
     e.preventDefault()
     
   persons.find(person=>person.name===newName)? 
-    window.alert(newName + " is already added to phonebook")
-   :setPersons(persons.concat({name: newName, number: newNumber}));
+  window.alert(newName + " is already added to phonebook")
+  :setPersons(persons.concat({name: newName, number: newNumber}));
   setNewName("")
   setNumber("")
   }
@@ -21,6 +22,9 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
+      <span>filter shown with</span>
+      <input value={filter} onChange={handleChange(setFilter)}/>
+      <h2>add a new</h2>
       <form onSubmit={addNewPerson}>
         <div>
           name: <input value={newName} onChange={handleChange(setNewName)}/>
@@ -34,7 +38,7 @@ const App = () => {
       </form>
       <h2>Numbers</h2>
       <div>
-        {persons.map(person => 
+        {persons.filter(person=> person.name.toLowerCase().includes(filter.toLowerCase())).map(person => 
           <div key={person.name}>{person.name} {person.number}</div> 
         )}
       </div>
