87c97e7910a77328d55a7176bddd86312535c880
teklitg <teklit.gebrer@gmail.com>
2023-10-27 00:42:06 -0700

    step 9

--- part3/Phonebackend/index.js
+++ part3/Phonebackend/index.js
@@ -83,7 +83,7 @@ app.post('/api/persons', (request, response) => {
   response.json(person)
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server is delivering its best on port ${PORT}`)
 })
