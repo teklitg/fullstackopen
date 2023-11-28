a9f6896a73f3bdbbb5f4be43ac59b830686de898
teklit gebrerufeal <teklit.gebrer@gmail.com>
2023-11-28 03:24:19 -0800

    4.4: helper functions and unit tests, step2

--- part4/Blog list/utils/ist_helper.js
+++ part4/Blog list/utils/ist_helper.js
@@ -3,6 +3,17 @@ const dummy = (blogs) => {
     return 1
   }
   
+  const totalLikes=(blogPosts)=> {
+    let total = 0;
+  
+    for (let i = 0; i < blogPosts.length; i++) {
+      const post = blogPosts[i];
+      total += post.likes;
+    }
+  
+    return total;
+  }
   module.exports = {
-    dummy
+    dummy,
+    totalLikes 
   }
