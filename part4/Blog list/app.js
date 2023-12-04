29ce3b523eb2ad47a007578b469ecdbe8fb433e5
teklit <teklit.gebrer@gmail.com>
2023-12-04 13:46:12 +0300

    4.15: bloglist expansion, step3

--- part4/Blog list/app.js
+++ part4/Blog list/app.js
@@ -5,6 +5,7 @@ const mongoose = require('mongoose')
 require("dotenv").config()
 const Blog = require('./model/model')
 const blogRouter= require('./controllers/blog')
+const userRouter = require('./controllers/user')
 const config = require('./utils/config')
 
 const mongoUrl = config.mongoUrl
@@ -14,5 +15,6 @@ mongoose.connect(mongoUrl)
 app.use(cors())
 app.use(express.json())
 app.use('/api/blogs',blogRouter)
+app.use('/api/users', userRouter)
 
 module.exports= app
---
+++ part4/Blog list/controllers/user.js
@@ -0,0 +1,35 @@
+const userRouter = require('express').Router()
+const User = require('../model/users')
+const bcrypt = require('bcrypt')
+
+
+userRouter.get('/', (request, response) => {
+    User
+      .find({})
+      .then(users => {
+        response.json(users)
+      })
+  })
+  
+  userRouter.post('/', async (request, response) => {
+  
+    const { username, name, password } = request.body
+
+    const saltRounds = 10
+    const passwordHash = await bcrypt.hash(password, saltRounds)
+    
+    const user = new User({
+        username,
+        name,
+        passwordHash,
+      })
+
+      console.log(user)
+      
+     const savUser= await user.save()
+      console.log(savUser)
+      response.status(201).json(savUser)
+  
+  })
+
+module.exports = userRouter
