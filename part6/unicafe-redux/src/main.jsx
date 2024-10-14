22ae3b203bc99344b5f0431fa5719de74c7eddd2
teklitdev <teklit.h.gebrerufael.com>
2024-10-14 17:18:01 +0300

    6.1: Unicafe Revisited, step 1

--- part6/unicafe-redux/src/main.jsx
+++ part6/unicafe-redux/src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 
 import { createStore } from 'redux'
-import reducer from './countReducer'
+import reducer from './reducer'
 
 const store = createStore(reducer)
 
