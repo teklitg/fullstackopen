56fa89a9497cb0cbccaa746d924cec12c852019f
teklitdev <teklit.h.gebrerufael.com>
2024-10-15 15:33:51 +0300

    step 9

--- part6/redux-anecdotes/src/components/AnecdoteList.jsx
+++ part6/redux-anecdotes/src/components/AnecdoteList.jsx
@@ -1,31 +1,29 @@
 import React from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 import { voteAnecdote } from '../reducers/anecdoteReducer'
-import { selectVisibleAnecdotes } from '../reducers/anecdoteReducer'
+import { selectVisibleAnecdotes } from '../reducers/anecdoteReducer' // Ensure this matches the export
 
 const AnecdoteList = () => {
-  const anecdotes = useSelector(selectVisibleAnecdotes); // Use the memoized selector
-  const dispatch = useDispatch()
+  const anecdotes = useSelector(selectVisibleAnecdotes);
+  const dispatch = useDispatch();
 
   const vote = (id) => {
-    dispatch(voteAnecdote(id))
-  }
+    dispatch(voteAnecdote(id));
+  };
 
   return (
     <div>
-      {anecdotes.map(anecdote =>
+      {anecdotes.map(anecdote => (
         <div key={anecdote.id}>
-          <div>
-            {anecdote.content}
-          </div>
+          <div>{anecdote.content}</div>
           <div>
             has {anecdote.votes}
             <button onClick={() => vote(anecdote.id)}>vote</button>
           </div>
         </div>
-      )}
+      ))}
     </div>
-  )
-}
+  );
+};
 
-export default AnecdoteList
+export default AnecdoteList;
