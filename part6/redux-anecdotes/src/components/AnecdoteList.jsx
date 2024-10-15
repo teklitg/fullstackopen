cf18812cd3f99a537cf6286acffa8a83e61b446a
teklitdev <teklit.h.gebrerufael.com>
2024-10-15 15:30:03 +0300

    step 8

--- part6/redux-anecdotes/src/components/AnecdoteList.jsx
+++ part6/redux-anecdotes/src/components/AnecdoteList.jsx
@@ -1,14 +1,10 @@
 import React from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 import { voteAnecdote } from '../reducers/anecdoteReducer'
+import { selectVisibleAnecdotes } from '../reducers/anecdoteReducer'
 
 const AnecdoteList = () => {
-  const anecdotes = useSelector(({ anecdotes, filter }) => {
-    return anecdotes.filter(anecdote =>
-      anecdote.content.toLowerCase().includes(filter.toLowerCase())
-    )
-  })
-
+  const anecdotes = useSelector(selectVisibleAnecdotes); // Use the memoized selector
   const dispatch = useDispatch()
 
   const vote = (id) => {
--- part6/redux-anecdotes/src/main.jsx
+++ part6/redux-anecdotes/src/main.jsx
@@ -1,11 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { createStore } from 'redux'
 import { Provider } from 'react-redux'
 import App from './App'
-import rootReducer from './reducers'
-
-const store = createStore(rootReducer)
+import store from './store'  // Import the store from store.js
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
